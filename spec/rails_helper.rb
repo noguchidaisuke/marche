1be920a3dd3e235e295f02e6fe8b1a1973b4a02a
github daisukenoguchi <daisukenoguemon@gmail.com>
2019-11-15 10:27:10 +0900

    ぐるなびAPIのrestaurantspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,10 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
-#require 'paperclip/matchers'
 RSpec.configure do |config|
   config.include Capybara::DSL
 end
@@ -25,7 +24,7 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -59,7 +58,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-  #config.include RequestSpecHelper, type: :request
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
--- spec/requests/restaurants_api_spec.rb
+++ spec/requests/restaurants_api_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
-
+require 'webmock/rspec'
 describe 'Restaurants API',type: :request do
-    it 'loads restaurants',vcr: true,focus: true do
+    it 'loads restaurants' do
         url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
         @freeword = '卵'
         params = {
             keyid: ENV['GURUNAVI_API_KEY'],
             freeword: @freeword
         }
-        binding.pry
         res=Faraday.get(url,params)
         expect(JSON.parse(res.body).length).to eq(5)
     end
