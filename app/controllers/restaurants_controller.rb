c3383d4fcf45089dac553d4480885fb211b8f389
github daisukenoguchi <daisukenoguemon@gmail.com>
2020-01-04 20:42:35 +0900

    修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,28 +1,28 @@
 class RestaurantsController < ApplicationController
   def new
-    @restaurants,@latitude,@longitude = [],[],[]
+    restaurants,@latitude,@longitude = [],[],[]
     url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
-    freeword = params[:freeword]
+    freeword = params[:freeword] unless params[:freeword] == ""
     query = {
-      keyid: ENV['GURUNAVI_API_KEY']
+      keyid: ENV['GURUNAVI_API_KEY'],
+      hit_per_page: 30
     }
-
-    if self.params[:area] ==""
+    if params[:area] == ""
       freeword = nil
     elsif params[:area] == "現在地"
-      latlng=params[:latlng]
-      latitude,longitude=latlng.scan(/[0-9]+.[0-9]+/)
-      current_point={latitude: latitude,
-        longitude: longitude,
-        range: 3
-      }
+      #navbarから現在地と入れられた場合の挙動
+      latitude,longitude=params[:latlng].scan(/[0-9]+.[0-9]+/)
+      if latitude.nil?
+        flash[:danger]="現在地を取得してください"
+        redirect_back(fallback_location: root_path)
+      end
+      current_point={latitude: latitude,longitude: longitude}
       query.merge!(current_point)
     else
       freeword+= ',' + params[:area]
       query[:freeword]=freeword
     end
     query.merge!({freeword: freeword})
-    binding.pry
     response = Faraday.get(url,query)
     response_json = JSON.parse(response.body)
     if response_json.present?
@@ -34,11 +34,13 @@ class RestaurantsController < ApplicationController
           else
             restaurant.save
           end
-          @restaurants << restaurant
-          @centerlat = @restaurants.first.latitude
-          @centerlong = @restaurants.first.longitude
+          restaurants << restaurant
         end
-      rescue
+        restaurants=restaurants.to_ary
+        @centerlat = restaurants.first.latitude
+        @centerlong = restaurants.first.longitude
+        @restaurants = Kaminari.paginate_array(restaurants,total_count: restaurants.count).page(1).per(10)
+      rescue => e
         flash[:danger]='該当のお店が見つかりませんでした。他のキーワードでお願いします'
         redirect_to root_path
       end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,8 @@
 class ToppagesController < ApplicationController
   def index
-    @restaurants = Restaurant.page(params[:page]).per(9)
+    #@restaurants = Restaurant.page(params[:page]).per(9)
+    restaurants = Restaurant.all.limit(50)
+    restaurants=restaurants.to_ary
+    @restaurants=Kaminari.paginate_array(restaurants,total_count: restaurants.count).page(1).per(12)
   end
 end
