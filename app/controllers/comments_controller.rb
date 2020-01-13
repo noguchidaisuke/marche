89ff09136fb2d22a37d66a8520d5d1ec4a443acc
github daisukenoguchi <daisukenoguemon@gmail.com>
2020-01-13 15:46:04 +0900

    star機能追加 restaurant new toppage

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,14 +16,14 @@ class CommentsController < ApplicationController
         end
     end
     def destroy
-        comment = Comment.find(params[:id])
+        comment = Comment.find(params[:restaurant_id])
         comment.destroy
         flash[:success] = "コメントを削除しました"
         redirect_back(fallback_location: root_path)
     end
 private
     def comment_params
-        params.require(:comment).permit(:restaurant_id,:user_id,:comment,:rating)
+        params.require(:comment).permit(:restaurant_id,:user_id,:comment,:rating,images:[])
     end
     def set_restaurant
       @restaurant = Restaurant.find(params[:restaurant_id])
